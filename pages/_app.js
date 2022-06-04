5c42263bc0106eb8ba3bca53204b3cc64cb1781e
bcecrypto <106697930+bcecrypto@users.noreply.github.com>
2022-06-03 22:58:38 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,8 @@ import { MoralisProvider } from 'react-moralis'
 function MyApp({ Component, pageProps }) {
   return (
     <MoralisProvider
-      serverUrl='https://igiyyigy3k3v.usemoralis.com:2053/server'
-      appId='7nLlid52Lpdw9QB2MViiResQu0zJpqPm5wJCpEpQ'
+      serverUrl=''
+      appId=''
     >
       <RobinhoodProvider>
         <Component {...pageProps} />
